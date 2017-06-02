bed0b6cfae0cae15ccff58939ba18059ab56149d
liushaofeng <jufengliushao@163.com>
2017-06-02 10:00:55 +0800

    add hanoi-methods

---
+++ Clist/SQHanoiFunction.c
@@ -0,0 +1,9 @@
+//
+//  SQHanoiFunction.c
+//  Clist
+//
+//  Created by cnlive-lsf on 2017/6/2.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#include "SQHanoiFunction.h"
---
+++ Clist/SQHanoiFunction.h
@@ -0,0 +1,14 @@
+//
+//  SQHanoiFunction.h
+//  Clist
+//
+//  Created by cnlive-lsf on 2017/6/2.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#ifndef SQHanoiFunction_h
+#define SQHanoiFunction_h
+
+#include <stdio.h>
+
+#endif /* SQHanoiFunction_h */
